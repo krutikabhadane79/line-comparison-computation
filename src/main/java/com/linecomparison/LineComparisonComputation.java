63fffe1594a54a301af24b245ab18ba8b8f4898e
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-09 14:36:55 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/main/java/com/linecomparison/LineComparisonComputation.java
@@ -0,0 +1,11 @@
+package com.linecomparison;
+
+import java.util.Scanner;
+
+public class LineComparisonComputation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program" );
+    }
+}
